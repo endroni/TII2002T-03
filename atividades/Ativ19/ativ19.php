7ea21fb96481d2b30ffc755a3b83f0299c6251c7
Rafael-Archanjo <70230261+Rafael-Archanjo@users.noreply.github.com>
2021-09-22 11:39:57 -0300

    Add files via upload

---
+++ atividades/Ativ19/ativ19.php
@@ -0,0 +1,10 @@
+<?php
+class Jogo
+{
+    public $nome;
+    protected $plataforma = "PC";
+    private $estilo;
+    var $ano;
+    var $graficos;
+}
+?>
---
+++ atividades/Ativ19/index.php
@@ -0,0 +1,16 @@
+<body>
+
+<?php
+require_once 'ativ19.php';
+$jogo1 = new Jogo();
+$jogo1->nome = "For The King";
+$jogo1->ano = "2017";
+$jogo1->graficos = "3D";
+
+echo "<pre>";
+print_r($jogo1);
+echo "</pre>"
+
+?>
+
+</body>
